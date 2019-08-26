3b420038322427a284660cd3c48b6a74e3647e4d
navasg <genenavas97@gmail.com>
2019-08-26 15:46:44 -0400

    Add responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,24 @@ $('.multiple-items').slick({
   infinite: true,
   slidesToShow: 5,
   dots: true,
-  slidesToScroll: 3
+  arrows: false,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 997,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+  ]
 });
 
 $(function () {
@@ -36,4 +53,30 @@ $('.testimonies').slick({
   fade: true,
   cssEase: 'linear',
   autoplay: true,
+
+});
+
+
+$('.carousel-service').slick({
+  arrows: true,
+  dots: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 997,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+  ]
 });
